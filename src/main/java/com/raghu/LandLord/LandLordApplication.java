5d262c4eccae854cf7b122dd5c87a65285619bc9
raghu8 <rvmanthena@gmail.com>
2020-06-26 23:33:36 -0400

    Initial Commit

---
+++ src/main/java/com/raghu/LandLord/LandLordApplication.java
@@ -0,0 +1,13 @@
+package com.raghu.LandLord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandLordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandLordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raghu/LandLord/LandLordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghu.LandLord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandLordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
